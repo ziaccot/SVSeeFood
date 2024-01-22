4164f54377c10ff810f1ddeb24a9068600e03c3f
ziaccot <ziaccot@gmail.com>
2024-01-22 11:19:38 +0200

    Created ConvNN model

---
+++ model_prep.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+# Create ConvNN model
+def create_model():
+    model = tf.keras.Sequential()
+
+    model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(224, 224, 3)))
+    model.add(tf.keras.layers.MaxPooling2D(2,2))
+
+    model.add(tf.keras.layers.Conv2D(64, (3,3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(2,2))
+
+    model.add(tf.keras.layers.Conv2D(128, (3, 3), activation='relu'))
+    model.add(tf.keras.layers.MaxPooling2D(2, 2))
+
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(128, activation='relu'))
+    model.add(tf.keras.layers.Dropout(0.2))
+
+    model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    model.summary()
+
+    return model
