27e88ce46c7250a970ff1eea7fb0a0ae9c0c8d46
ziaccot <ziaccot@gmail.com>
2024-01-30 12:09:23 +0200

    fixing script to main pattern

--- training.py
+++ training.py
@@ -5,21 +5,26 @@ from matplotlib import pyplot as plt
 
 EPOCHS = 20 # num of epochs
 
-create_dataset()
-training_generator, validation_generator = get_generators()
-model = create_model()
+# Training model and saving it to local drive
+def train_model():
+    create_dataset()
+    training_generator, validation_generator = get_generators()
+    model = create_model()
 
-history = model.fit(training_generator,
-                    epochs=EPOCHS,
-                    validation_data=validation_generator)
+    history = model.fit(training_generator,
+                        epochs=EPOCHS,
+                        validation_data=validation_generator)
 
-# Plotting the training history
-plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
-plt.xlabel('Epoch')
-plt.ylabel('Accuracy')
-plt.legend()
-plt.show()
+    # Plotting the training history
+    plt.plot(history.history['accuracy'], label='accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.show()
 
-# Saving the model
-model.save('hotdog_not_hotdog_model.h5')
+    # Saving the model
+    model.save('hotdog_not_hotdog_model.h5')
+
+if __name__ == '__main__':
+    train_model()
